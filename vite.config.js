731da95f73e6c68a13f8342538f0deb1f77f2ae3
Shuayb (Jet) <jethrofj12@gmail.com>
2025-08-13 21:38:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: Change base to '/<your-repo-name>/' when deploying to GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: '/Trojan-HIV/',
+  base: '/Trojan-HIV-/',   // <-- must match repo name EXACTLY (incl. trailing dash)
 })
